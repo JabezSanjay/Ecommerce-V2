91769a9945fa8cdebc75194c875a738483d8ef08
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-14 08:11:37 +0530

    Completed basic node setup

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const localDb = process.env.DATABASE;
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//DB Connection
+mongoose
+  .connect(localDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log(err));
+
+//Port
+const port = process.env.port || 8000;
+
+//Server Start
+app.listen(port, () => {
+  console.log(`App is running at ${port}`);
+});
