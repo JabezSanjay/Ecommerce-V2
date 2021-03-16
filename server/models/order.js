f3038859208d63c1af5041299800f31249a0f1e0
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-16 21:40:49 +0530

    Completed order schmea

--- server/models/order.js
+++ server/models/order.js
@@ -12,3 +12,26 @@ const ProductCartSchema = new mongoose.Schema({
 });
 
 const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
+
+const OrderSchema = new mongoose.Schema(
+  {
+    products: [ProductCartSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: String,
+    status: {
+      type: String,
+      default: "Recieved",
+      enum: ["Cancelled", "Delivered", "Shipped", "Processing", "Recieved"],
+    },
+    user: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = { Order, ProductCart };
