ab38ab5b6e2ea85d6781c187e531ab5f35da698c
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-16 21:31:57 +0530

    Completed Product Cart Schema

---
+++ server/models/order.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const ProductCartSchema = new mongoose.Schema({
+  product: {
+    type: ObjectId,
+    ref: "Product",
+  },
+  name: String,
+  count: Number,
+  price: Number,
+});
+
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
