6fdb6005b53a90fbd96b67293ea0ff99a6d90f6b
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-18 20:50:53 +0530

    Completed isAdmin middleware

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -75,3 +75,12 @@ exports.isAuthenticated = (req, res, next) => {
   }
   next();
 };
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "Access Denied!",
+    });
+  }
+  next();
+};
