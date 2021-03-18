921c4e38153e4781ac6da8aebd1e0ce6474d49ae
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-18 12:38:35 +0530

    Completed user signin

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 exports.register = (req, res) => {
   const errors = validationResult(req);
@@ -20,6 +22,38 @@ exports.register = (req, res) => {
       name: user.name,
       email: user.email,
       id: user._id,
+    });
+  });
+};
+
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
+  User.findOne({ email }, (error, user) => {
+    if (error || !user) {
+      return res.status(400).json({
+        error: "User does not exist!",
+      });
+    }
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and password do not match!",
+      });
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    res.cookie("token", token);
+
+    const { _id, name, email, role } = user;
+    return res.json({
+      token,
+      user: { _id, name, email, role },
     });
   });
 };
