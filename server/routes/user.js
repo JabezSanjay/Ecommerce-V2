024a33a130cf662b19501f4ba547d0e1e187cfab
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-20 21:40:27 +0530

    Completed userPurchaseList route and controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser, updateUser } = require("../controllers/user");
+const {
+  getUserById,
+  getUser,
+  updateUser,
+  userPurchaseList,
+} = require("../controllers/user");
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
 
 router.param("userId", getUserById);
@@ -9,5 +14,12 @@ router.param("userId", getUserById);
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+
+router.get(
+  "/user/orders/:userId",
+  isSignedIn,
+  isAuthenticated,
+  userPurchaseList
+);
 
 module.exports = router;
