0b5ac6db50070bd6b4b20e58455c3250f966a086
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-21 22:08:19 +0530

    Completed getCategory route and controller

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,5 +1,17 @@
 const Category = require("../models/category");
 
+exports.getCategoryById = (req, res, next, id) => {
+  Category.findById(id).exec((error, category) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Category not found!",
+      });
+    }
+    req.category = category;
+    next();
+  });
+};
+
 exports.createCategory = (req, res) => {
   const category = new Category(req.body);
   category.save((error, category) => {
@@ -23,4 +35,8 @@ exports.getAllCategories = (req, res) => {
     }
     return res.json(categories);
   });
+};
+
+exports.getCategory = (req, res) => {
+  return res.json(req.category);
 };
