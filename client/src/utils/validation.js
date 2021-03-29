662ae6a2c4336b648937fe22304a645193ae6ce1
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-03-29 21:49:24 +0530

    Completed Validation.js

--- client/src/utils/validation.js
+++ client/src/utils/validation.js
@@ -2,13 +2,7 @@ import * as Yup from "yup";
 
 export const signUpValidationSchema = Yup.object({
   name: Yup.string().required("User name is required"),
-  email: Yup.string()
-    .email()
-    .matches(
-      /^(?=.*[a-zA-Z0-9])[a-zA-Z0-9!@#$%&*.]{7,}$/,
-      "Please enter a valid email!"
-    )
-    .required("Email is required"),
+  email: Yup.string().email().required("Email is required"),
   password: Yup.string().required("Password is required"),
 });
 
