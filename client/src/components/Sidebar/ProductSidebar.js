d0fb6f07e23c995935a07918120bb843ea91910e
Jabez Sanjay <jbzsanjay2000@gmail.com>
2021-04-10 23:07:45 +0530

    Completed input layout of productSidebar

--- client/src/components/Sidebar/ProductSidebar.js
+++ client/src/components/Sidebar/ProductSidebar.js
@@ -1,9 +1,33 @@
 import React from "react";
-import { Drawer, Form, Input } from "antd";
+import {
+  Drawer,
+  Form,
+  Input,
+  Upload,
+  message,
+  Button,
+  Select,
+  InputNumber,
+} from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+
+const { Option } = Select;
 
 const ProductSidebar = ({ visible, onClose }) => {
   const Close = (value) => {
     onClose(value);
+  };
+  const props = {
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        message.loading("Loading!");
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
   };
   return (
     <>
@@ -21,40 +45,75 @@ const ProductSidebar = ({ visible, onClose }) => {
         >
           <Form.Item
             name="name"
-            label="Category name"
+            label="Product name"
             rules={[{ required: true, message: "Please enter a name!" }]}
           >
             <Input
               name="name"
-              placeholder="Enter a category name"
+              placeholder="Enter a product name"
               onChange={(e) => {
                 // setName(e.target.value);
               }}
             />
           </Form.Item>
-          {/* <Form.Item>
-        {create ? (
-          <Button
-            type="primary"
-            size="large"
-            htmlType="submit"
-            loading={loading}
-            block
+
+          <Form.Item
+            name="category"
+            label="Product category"
+            rules={[{ required: true, message: "Please select a category!" }]}
           >
+            <Select style={{ width: 250 }}>
+              <Option value="jack">Jack</Option>
+              <Option value="lucy">Lucy</Option>
+              <Option value="disabled" disabled>
+                Disabled
+              </Option>
+              <Option value="Yiminghe">yiminghe</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            name="price"
+            label="Product price"
+            rules={[{ required: true, message: "Please enter a price!" }]}
+          >
+            <InputNumber
+              style={{ width: 250 }}
+              min={1}
+              max={10000}
+              defaultValue={400}
+            />
+          </Form.Item>
+          <Form.Item
+            name="stock"
+            label="Product stock"
+            rules={[{ required: true, message: "Please enter a stock!" }]}
+          >
+            <InputNumber
+              style={{ width: 250 }}
+              min={1}
+              max={100}
+              defaultValue={30}
+            />
+          </Form.Item>
+          <Form.Item
+            name="image"
+            label="Product image"
+            rules={[{ required: true, message: "Please upload an image!" }]}
+          >
+            <Upload {...props}>
+              <Button
+                icon={<UploadOutlined />}
+                size="large"
+                htmlType="submit"
+                style={{ width: 250 }}
+              >
+                Click to Upload
+              </Button>
+            </Upload>
+          </Form.Item>
+          <Button type="primary" block size="large" htmlType="submit">
             Create
           </Button>
-        ) : (
-          <Button
-            type="primary"
-            size="large"
-            htmlType="submit"
-            loading={loading}
-            block
-          >
-            Edit
-          </Button>
-        )}
-      </Form.Item> */}
         </Form>
       </Drawer>
       {/* {error && message.error(error)} */}
